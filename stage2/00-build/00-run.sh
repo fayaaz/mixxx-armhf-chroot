5223311358a4bf823419ebfbabd1e3e3c44dd9ce
Fayaaz <fayaaz.ahmed@gmail.com>
2020-08-01 21:48:33 +0100

    Uncomment git checkout mixxx

--- stage2/00-build/00-run.sh
+++ stage2/00-build/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 on_chroot << EOF
-    # git clone https://github.com/mixxxdj/mixxx.git /code/
+    git clone https://github.com/mixxxdj/mixxx.git /code/
     cd /code/ && git checkout 2.3
     cd /code/ && scons -j4 optimize=native opengles=1 machine=armhf faad=1
 EOF
