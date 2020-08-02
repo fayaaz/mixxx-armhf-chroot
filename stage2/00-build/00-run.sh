541f848854a55ea0934621bfe912281c26ba8fe6
Fayaaz Ahmed <fayaaz@users.noreply.github.com>
2020-08-02 01:02:22 +0100

    Optimize=portable for all pis

--- stage2/00-build/00-run.sh
+++ stage2/00-build/00-run.sh
@@ -2,5 +2,6 @@
 on_chroot << EOF
     git clone https://github.com/mixxxdj/mixxx.git /code/
     cd /code/ && git checkout 2.3
-    cd /code/ && scons -j4 optimize=native opengles=1 machine=armhf faad=1
+    cd /code/ && scons -j4 optimize=portable opengles=1 machine=armhf faad=1
+    cd /code/ && scons -j4 makeubuntu    
 EOF
