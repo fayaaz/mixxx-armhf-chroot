bdc71266d7f4c9049b80c6a533f43a71a6691571
Fayaaz <fayaaz.ahmed@gmail.com>
2020-08-02 07:09:23 +0100

    Try using cmake instead of scons

--- stage2/00-build/00-run.sh
+++ stage2/00-build/00-run.sh
@@ -2,5 +2,10 @@
 on_chroot << EOF
     git clone https://github.com/mixxxdj/mixxx.git /code/
     cd /code/ && git checkout 2.3
-    cd /code/ && scons -j4 optimize=native opengles=1 machine=armhf faad=1
+    export CMAKE_BUILD_PARALLEL_LEVEL="$(nproc)"
+    export PATH="$HOME/.local/bin:$PATH"
+    mkdir cmake_build && cd cmake_build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+    cmake --build .
+    cpack -G DEB
 EOF
